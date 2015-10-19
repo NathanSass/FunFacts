ffbe1f2fd3d62c5d3bb430595a4650910714d756
Nathan Sass <nsass711@gmail.com>
2015-10-19 11:41:01 -0700

    cycles through 10 facts

--- app/src/main/java/com/nathansass/funfacts/FunFacts.java
+++ app/src/main/java/com/nathansass/funfacts/FunFacts.java
@@ -14,6 +14,18 @@ public class FunFacts extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final String[] facts = {
+                "Ants stretch when they wake up in the morning.",
+                "Ostriches can run faster than horses.",
+                "Olympic gold medals are actually made mostly of silver.",
+                "You are born with 300 bones; by the time you are an adult you will have 206.",
+                "It takes about 8 minutes for light from the Sun to reach Earth.",
+                "Some bamboo plants can grow almost a meter in just one day.",
+                "The state of Florida is bigger than England.",
+                "Some penguins can leap 2-3 meters out of the water.",
+                "On average, it takes 66 days to form a new habit.",
+                "Mammoths still walked the earth when the Great Pyramid was being built." };
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
 
@@ -27,8 +39,9 @@ public class FunFacts extends AppCompatActivity {
                 // button clicked
                 String fact = "";
                 Random randomGenerator = new Random();
-                int randomNumber = randomGenerator.nextInt(3);
-                fact = randomNumber + "";
+                int randomNumber = randomGenerator.nextInt(facts.length);
+
+                fact = facts[randomNumber];
 
                 factlabel.setText(fact);
             }
